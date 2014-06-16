b0f4b77ce6773c34bf5e5b72c0e4e0b493bdc0d6
Rikki Katz <katz.rikki@gmail.com>
2014-06-16 15:40:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rikkikatz::Application.config.secret_key_base = 'eb16ff9585fedb19255c17be021fc9a7d4825d2484ded14c7b91a7e6907db748f81dd25ad2322556fa1ba7bafe41364013f94660c96e5836e9bde8e35157eab2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rikkikatz::Application.config.session_store :cookie_store, key: '_rikkikatz_session'
